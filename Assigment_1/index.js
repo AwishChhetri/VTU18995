3d1ea624221daf53c7688ee69bb38421b31d4fc7
ABISHCHHETRI <165697751+ABISHCHHETRI@users.noreply.github.com>
2024-05-01 13:19:41 +0530

    final

--- Assigment_1/index.js
+++ Assigment_1/index.js
@@ -1,42 +1,37 @@
 const express = require('express');
-const axios = require('axios');
-const { v4: uuidv4 } = require('uuid');
-
 const app = express();
-const PORT = process.env.PORT || 3000;
+const cors = require('cors');
+const axios = require('axios');
+const bodyParser = require('body-parser'); // Import body-parser
 
+app.use(cors());
+app.use(bodyParser.json()); // Add body-parser middleware
 
-  
+app.listen(8000, () => {
+    console.log('Server running at port 8000');
+});
 
-// // GET /companies/:companyName/categories/:categoryName/products/:productType
-app.get('/companies/:companyName/categories/:categoryName/products/:productType', async (req, res) => {
+app.post('/product', async (req, res) => {
+    console.log(req.body)
+    const cat = req.body.category;
+   
+    const API = `http://20.244.56.144/test/companies/FLP/categories/${cat}/products?top=10&minPrice=1&maxPrice=1000`;
+    console.log(API)
+    const params = {
+        top: 10,
+        minPrice: 1,
+        maxPrice: 1000
+    };
+    const bearerToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE0NTQ5ODM4LCJpYXQiOjE3MTQ1NDk1MzgsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6Ijk4ZTAxOTc0LTdmMGMtNDE0Yi1iNjJmLTYyMThmMjBjN2FmNCIsInN1YiI6InZ0dTE4OTk1QHZlbHRlY2guZWR1LmluIn0sImNvbXBhbnlOYW1lIjoiZ29NYXJ0IiwiY2xpZW50SUQiOiI5OGUwMTk3NC03ZjBjLTQxNGItYjYyZi02MjE4ZjIwYzdhZjQiLCJjbGllbnRTZWNyZXQiOiJjRUtOWFNaVFl4YWZaY3J1Iiwib3duZXJOYW1lIjoiQWJpc2ggQ2hoZXRyaSIsIm93bmVyRW1haWwiOiJ2dHUxODk5NUB2ZWx0ZWNoLmVkdS5pbiIsInJvbGxObyI6IjE4OTk1In0.cxsvUZAnwOmdZPRkcP867USOq9lC3vf_xFKTXj9eU4Q';
+    const headers = {
+        Authorization: `Bearer ${bearerToken}`
+    };
     try {
-      const { companyName, categoryName, productType } = req.params;
-      const { top, minPrice, maxPrice } = req.query;
-      const bearerToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE0NTQ1ODAxLCJpYXQiOjE3MTQ1NDU1MDEsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6Ijk4ZTAxOTc0LTdmMGMtNDE0Yi1iNjJmLTYyMThmMjBjN2FmNCIsInN1YiI6InZ0dTE4OTk1QHZlbHRlY2guZWR1LmluIn0sImNvbXBhbnlOYW1lIjoiZ29NYXJ0IiwiY2xpZW50SUQiOiI5OGUwMTk3NC03ZjBjLTQxNGItYjYyZi02MjE4ZjIwYzdhZjQiLCJjbGllbnRTZWNyZXQiOiJjRUtOWFNaVFl4YWZaY3J1Iiwib3duZXJOYW1lIjoiQWJpc2ggQ2hoZXRyaSIsIm93bmVyRW1haWwiOiJ2dHUxODk5NUB2ZWx0ZWNoLmVkdS5pbiIsInJvbGxObyI6IjE4OTk1In0.BV_FX8EbCllcfzkymC83efVD6l8TyFVVYxOpHvbgGDc';
-  
-      // Constructing headers with the bearer token
-      const headers = {
-        'Authorization': `Bearer ${bearerToken}`
-      };
-  
-      const response = await axios.get(`http://20.244.56.144/test/companies/${companyName}/categories/${categoryName}/products/${productType}`, {
-        params: {
-          top,
-          minPrice,
-          maxPrice
-        },
-        headers: headers 
-      });
-  
-      res.json(response.data.products);
+        const result = await axios.get(API, { headers, params });
+        console.log(result)
+        res.json(result.data).status(200); 
     } catch (error) {
-      console.error('Error fetching products:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+        console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-  });
-  
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
